3cfa9337a07c212e767d4666f1c3ed62e5234cc0
alessandrocls <alessandro.christopher@gmail.com>
2018-01-23 12:25:34 +0700

    Back End

--- Back End/models/user.js
+++ Back End/models/user.js
@@ -5,6 +5,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/bootcamp");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    email: String,
     username : String,
     password : String
 });
